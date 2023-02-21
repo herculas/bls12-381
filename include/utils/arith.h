dd73ffb28176f2f7ed6f089fd871fb40d746448b
Jon <ruisong20@gmail.com>
2023-02-21 20:59:22 +0800

    [fix] comments

--- include/utils/arith.h
+++ include/utils/arith.h
@@ -5,21 +5,21 @@
 
 namespace bls12_381::util::arithmetic {
 
-/// Computes a + b + carry, returning the result and the new carry over.
+/// Computes a + b + carry, returning the result and updating the carry.
 constexpr inline uint64_t adc(uint64_t a, uint64_t b, uint64_t &carry) {
     auto res = static_cast<__uint128_t>(a) + static_cast<__uint128_t>(b) + static_cast<__uint128_t>(carry);
     carry = static_cast<uint64_t>((res >> 64));
     return static_cast<uint64_t>(res);
 }
 
-/// Computes a - (b + borrow), returning the result and the new borrow.
+/// Computes a - (b + borrow), returning the result and updating the borrow.
 constexpr inline uint64_t sbb(uint64_t a, uint64_t b, uint64_t &borrow) {
     auto res = static_cast<__uint128_t>(a) - static_cast<__uint128_t>(b) - static_cast<__uint128_t>(borrow >> 63);
     borrow = static_cast<uint64_t>(res >> 64);
     return static_cast<uint64_t>(res);
 }
 
-/// Computes a + (b * c) + carry, returning the result and the new carry over.
+/// Computes a + (b * c) + carry, returning the result and updating the carry.
 constexpr inline uint64_t mac(uint64_t a, uint64_t b, uint64_t c, uint64_t &carry) {
     auto res = static_cast<__uint128_t>(a) + (static_cast<__uint128_t>(b) * static_cast<__uint128_t>(c)) +
                static_cast<__uint128_t>(carry);
--- include/utils/encode.h
+++ include/utils/encode.h
@@ -6,18 +6,19 @@
 
 namespace bls12_381::util::encoding {
 
-inline std::string hex_str(const std::span<uint8_t> span) {
+constexpr char hexMap[16] = {
+        '0', '1', '2', '3',
+        '4', '5', '6', '7',
+        '8', '9', 'a', 'b',
+        'c', 'd', 'e', 'f'
+};
+
+inline std::string hex_str(const std::span<uint8_t> digits) {
     std::string result;
-    static constexpr char hexMap[16] = {
-            '0', '1', '2', '3',
-            '4', '5', '6', '7',
-            '8', '9', 'a', 'b',
-            'c', 'd', 'e', 'f'
-    };
-    result.reserve(span.size() * 2);
-    for (uint8_t v: span) {
-        result.push_back(hexMap[v >> 4]);
-        result.push_back(hexMap[v & 15]);
+    result.reserve(digits.size() * 2);
+    for (uint8_t digit: digits) {
+        result.push_back(hexMap[digit >> 4]);
+        result.push_back(hexMap[digit & 15]);
     }
     return result;
 }
